065ad5270b5c94c7a2b06f3187f29b9b67ee04b1
Daniel Allen <dan@aeternagroup.net>
2026-08-15 01:15:52 +0100

    Update to schema version v5.0.0

--- outputs.tf
+++ outputs.tf
@@ -4,7 +4,7 @@ output "managed_devops_pools_id" {
 }
 output "managed_devops_pools_azure_devops_organization" {
   description = "Map of azure_devops_organization values across all managed_devops_pools, keyed the same as var.managed_devops_pools"
-  value       = { for k, v in azurerm_managed_devops_pool.managed_devops_pools : k => v.azure_devops_organization if v.azure_devops_organization != null && length(v.azure_devops_organization) > 0 }
+  value       = { for k, v in azurerm_managed_devops_pool.managed_devops_pools : k => one(v.azure_devops_organization) if v.azure_devops_organization != null && length(v.azure_devops_organization) > 0 }
 }
 output "managed_devops_pools_dev_center_project_id" {
   description = "Map of dev_center_project_id values across all managed_devops_pools, keyed the same as var.managed_devops_pools"
@@ -12,7 +12,7 @@ output "managed_devops_pools_dev_center_project_id" {
 }
 output "managed_devops_pools_identity" {
   description = "Map of identity values across all managed_devops_pools, keyed the same as var.managed_devops_pools"
-  value       = { for k, v in azurerm_managed_devops_pool.managed_devops_pools : k => v.identity if v.identity != null && length(v.identity) > 0 }
+  value       = { for k, v in azurerm_managed_devops_pool.managed_devops_pools : k => one(v.identity) if v.identity != null && length(v.identity) > 0 }
 }
 output "managed_devops_pools_location" {
   description = "Map of location values across all managed_devops_pools, keyed the same as var.managed_devops_pools"
@@ -32,11 +32,11 @@ output "managed_devops_pools_resource_group_name" {
 }
 output "managed_devops_pools_stateful_agent" {
   description = "Map of stateful_agent values across all managed_devops_pools, keyed the same as var.managed_devops_pools"
-  value       = { for k, v in azurerm_managed_devops_pool.managed_devops_pools : k => v.stateful_agent if v.stateful_agent != null && length(v.stateful_agent) > 0 }
+  value       = { for k, v in azurerm_managed_devops_pool.managed_devops_pools : k => one(v.stateful_agent) if v.stateful_agent != null && length(v.stateful_agent) > 0 }
 }
 output "managed_devops_pools_stateless_agent" {
   description = "Map of stateless_agent values across all managed_devops_pools, keyed the same as var.managed_devops_pools"
-  value       = { for k, v in azurerm_managed_devops_pool.managed_devops_pools : k => v.stateless_agent if v.stateless_agent != null && length(v.stateless_agent) > 0 }
+  value       = { for k, v in azurerm_managed_devops_pool.managed_devops_pools : k => one(v.stateless_agent) if v.stateless_agent != null && length(v.stateless_agent) > 0 }
 }
 output "managed_devops_pools_tags" {
   description = "Map of tags values across all managed_devops_pools, keyed the same as var.managed_devops_pools"
@@ -44,7 +44,7 @@ output "managed_devops_pools_tags" {
 }
 output "managed_devops_pools_virtual_machine_scale_set_fabric" {
   description = "Map of virtual_machine_scale_set_fabric values across all managed_devops_pools, keyed the same as var.managed_devops_pools"
-  value       = { for k, v in azurerm_managed_devops_pool.managed_devops_pools : k => v.virtual_machine_scale_set_fabric if v.virtual_machine_scale_set_fabric != null && length(v.virtual_machine_scale_set_fabric) > 0 }
+  value       = { for k, v in azurerm_managed_devops_pool.managed_devops_pools : k => one(v.virtual_machine_scale_set_fabric) if v.virtual_machine_scale_set_fabric != null && length(v.virtual_machine_scale_set_fabric) > 0 }
 }
 output "managed_devops_pools_work_folder" {
   description = "Map of work_folder values across all managed_devops_pools, keyed the same as var.managed_devops_pools"
